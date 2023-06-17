30a5bd26a5611382eaac3e321350d6e202418410
Ziemowit Łąski <15880281+zlaski@users.noreply.github.com>
2023-06-17 01:59:26 +0000

    Delete scripts

--- dry-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-sudo certbot certonly -v --dry-run -a dns-zoneedit --dns-zoneedit-propagation-seconds 120 \
-    -d 'akisystems.com' -d '*.akisystems.com'  -d '*.hcsv.akisystems.com'
--- wet-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-sudo rm -rf /etc/letsencrypt/live/aki*
-sudo certbot -v -a dns-zoneedit -i nginx --dns-zoneedit-propagation-seconds 120 \
-    -m 'zlaski@ziemas.net' --agree-tos --non-interactive \
-    -d 'akisystems.com' -d '*.akisystems.com'  -d '*.hcsv.akisystems.com'
